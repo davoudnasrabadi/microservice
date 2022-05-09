76b282a35b9271a3da5f68fb46ce04f3b03abe88
davoudnasrabadi <davoudnsr90@gmail.com>
2022-05-09 12:16:04 +0430

    createDto added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post,Get, Body, Param, Delete, Put, UsePipes } from '@nestjs/common';
+import { GrpcMethod} from '@nestjs/microservices';
+import {CreateUserDto} from '../Dto/user.dto';
+import { UserService } from './user.service';
+import { UserById } from './interfaces';
+import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
+import { User } from '../model/user.entity';
+import {Observable} from 'rxjs';
+import {ValidationPipe} from './myPipe';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    constructor(private userService:UserService){}
+    
+    @GrpcMethod('UsersService', 'CreateOne')
+    create(data:any){
+        console.log(data);
+        return this.userService.createUser(data);
+    }
+    
+    @GrpcMethod('UsersService', 'FindAll')
+    getAll(data:any){
+          return this.userService.getAll();
+    }
+    @GrpcMethod('UsersService', 'FindOne')
+    getById(id:any):any{
+        const i=id.id;
+        return this.userService.getById(i);
+    }
+    
+    @GrpcMethod('UsersService', 'DeleteOne')
+    deleteById(id:any){
+        const i = id.id;
+        return this.userService.deleteById(i);
+    }
+
+    @GrpcMethod('UsersService', 'UpdateOne')
+    updateUser(data:any){
+        const id = data.id;
+        const body = {
+            username:data.username,
+            email:data.email,
+            password:data.password
+        }
+       return this.userService.updateUser(id,body);
+    }
+
+}
