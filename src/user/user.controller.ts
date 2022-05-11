438733013dae28e487b05d61075870701fc97d9a
davoudnasrabadi <davoudnsr90@gmail.com>
2022-05-11 15:41:25 +0430

    logger added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post,Get, Body, Param, Delete, Put, UsePipes } from '@nestjs/common';
+import { Controller, Post,Get, Body, Param, Delete, Put, UsePipes,Logger } from '@nestjs/common';
 import { GrpcMethod} from '@nestjs/microservices';
 import {CreateUserDto} from '../Dto/createUser.dto';
 import { UserService } from './user.service';
@@ -12,26 +12,30 @@ import {deleteMsg,updateMsg,AllMsg,UserByIdDto,errorMsg} from '../Dto/responseDt
 @Controller('users')
 export class UserController {
     constructor(private userService:UserService){}
-    
+    private readonly logger = new Logger(UserController.name);
     
     @GrpcMethod('UsersService', 'FindAll')
     getAll():Promise<AllMsg>{
+          this.logger.log("Inside getAll of microservice....")
           return this.userService.getAll();
     }
     @GrpcMethod('UsersService', 'FindOne')
     getById(id:any):Promise<UserByIdDto | errorMsg>{
+        this.logger.log("Inside getById of microservice....")
         const i=id.id;
         return this.userService.getById(i);
     }
     
     @GrpcMethod('UsersService', 'DeleteOne')
     deleteById(id:any):Promise<deleteMsg>{
+        this.logger.log("Inside deleteById of microservice....")
         const i = id.id;
         return this.userService.deleteById(i);
     }
 
     @GrpcMethod('UsersService', 'UpdateOne')
     updateUser(data:any):Promise<updateMsg>{
+        this.logger.log("Inside updateUser of microservice....")
         const id = data.id;
         const body = {
             username:data.username,
